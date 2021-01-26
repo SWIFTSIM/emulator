81f81fdb666d2569f24c10e1b3ee5ec0a4161f25
Josh Borrow <joshua.borrow@durham.ac.uk>
2021-01-26 13:20:25 +0000

    Added test for number of variables

--- tests/test_model_values.py
+++ tests/test_model_values.py
@@ -27,4 +27,30 @@ def test_basic_model_values():
         2: {"independent": np.arange(10), "dependent": np.random.rand(10),},
     }
 
-    ModelValues(model_values=input_model_values)
+    output_model = ModelValues(model_values=input_model_values)
+
+    assert output_model.number_of_variables == 30
+
+
+def test_basic_model_values_raise():
+    """
+    A basic test of model values, with incorrect data
+    that raises an exception.
+    """
+
+    input_model_values = {
+        0: {
+            "independent": np.arange(10),
+            "dependent": np.random.rand(10),
+            "dependent_error": np.random.rand(10),
+        },
+        1: {
+            "independent": np.arange(10),
+            "dependent": np.random.rand(10),
+            "dependent_error": np.random.rand(18).reshape(9, 2),
+        },
+        2: {"independent": np.arange(10), "dependent": np.random.rand(10),},
+    }
+
+    with pytest.raises(AttributeError):
+        ModelValues(model_values=input_model_values)
