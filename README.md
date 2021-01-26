54118e3c634084af0e64e2aed8fcb0dc5c63adf0
Josh Borrow <joshua.borrow@durham.ac.uk>
2021-01-26 10:12:59 +0000

    Added base files

---
+++ README.md
@@ -0,0 +1,21 @@
+SWIFT Emulator
+==============
+
+Emulator for SWIFT run outputs, used to predict outputs of simulations
+without having to run them. Employs Gaussian Process Regression with
+`george` and sensitivity analysis with `SALib`.
+
+Authors:
+
++ Roi Kugel
++ Josh Borrow
+
+
+Requirements
+------------
+
++ `george`
++ `SALib`
++ `attrs`
++ `numpy`
++ `unyt`
---
+++ setup.py
@@ -0,0 +1,28 @@
+import setuptools
+from swiftpipeline import __version__
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="swiftemulator",
+    version=__version__,
+    description="Emulator for interpolating SWIFT runs.",
+    url="https://github.com/SWIFTSIM/emulator",
+    author="Josh Borrow",
+    author_email="josh@joshborrow.com",
+    packages=setuptools.find_packages(),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    zip_safe=False,
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=["numpy", "unyt", "attrs", "george", "SALib",],
+    include_package_data=True,
+)
