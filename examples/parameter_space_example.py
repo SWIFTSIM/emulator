5b8c56c5543af65068dc0296825081d1a14fe9c7
Josh Borrow <joshua.borrow@durham.ac.uk>
2021-02-08 14:49:42 +0000

    Added example plotting emulator design

---
+++ examples/parameter_space_example.py
@@ -0,0 +1,46 @@
+"""
+An example using the SWIFT i/o functions to demonstrate how a 
+parameter space is filled.
+
+``http://virgodb.cosma.dur.ac.uk/swift-webstorage/IOExamples/emulator_output.zip``
+"""
+
+from swiftemulator.io.swift import load_parameter_files
+from glob import glob
+from pathlib import Path
+
+files = [Path(x) for x in glob("./emulator_output/input_data/*.yml")]
+
+filenames = {filename.stem: filename for filename in files}
+
+spec, parameters = load_parameter_files(
+    filenames=filenames,
+    parameters=[
+        "EAGLEFeedback:SNII_energy_fraction_min",
+        "EAGLEFeedback:SNII_energy_fraction_max",
+        "EAGLEFeedback:SNII_energy_fraction_n_Z",
+        "EAGLEFeedback:SNII_energy_fraction_n_0_H_p_cm3",
+        "EAGLEFeedback:SNII_energy_fraction_n_n",
+        "EAGLEAGN:coupling_efficiency",
+        "EAGLEAGN:viscous_alpha",
+        "EAGLEAGN:AGN_delta_T_K",
+    ],
+    log_parameters=[
+        "EAGLEAGN:AGN_delta_T_K",
+        "EAGLEAGN:viscous_alpha",
+        "EAGLEAGN:coupling_efficiency",
+        "EAGLEFeedback:SNII_energy_fraction_n_0_H_p_cm3",
+    ],
+    parameter_printable_names=[
+        "$f_{\\rm E, min}$",
+        "$f_{\\rm E, max}$",
+        "$n_{Z}$",
+        "$\\log_{10}$ $n_{\\rm H, 0}$",
+        "$n_{n}$",
+        "$\\log_{10}$ $C_{\\rm eff}$",
+        "$\\log_{10}$ $\\alpha_{\\rm V}$",
+        "AGN $\\log_{10}$ $\\Delta T$",
+    ],
+)
+
+parameters.plot_model(model_specification=spec, filename="parameter_space.png")
