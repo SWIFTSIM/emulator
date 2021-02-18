62da8fde27be003cb13056e302f239eba8163569
Moyoxkit <Roikugul@hotmail.com>
2021-02-18 14:11:48 +0100

    Port syle changes to binned cross check + add test

--- swiftemulator/sensitivity/cross_check_bins.py
+++ swiftemulator/sensitivity/cross_check_bins.py
@@ -12,6 +12,7 @@ import sklearn.linear_model as lm
 from sklearn.preprocessing import PolynomialFeatures
 from sklearn.pipeline import Pipeline
 from tqdm import tqdm
+from pathlib import Path
 
 from swiftemulator.backend.model_parameters import ModelParameters
 from swiftemulator.backend.model_values import ModelValues
@@ -61,12 +62,39 @@ class CrossCheckBins(object):
         fit_model: str = "none",
         lasso_model_alpha: float = 0.0,
         polynomial_degree: int = 1,
+        hide_progress: bool = True,
     ):
         """
         Build a dictonary with an emulator for each simulation
         where the data of that simulation is left out
 
         Note: this can take a long time
+
+        Parameters
+        ----------
+
+        kernel, george.kernels
+            The ``george`` kernel to use. The GPE here uses a copy
+            of this instance. By default, this is the
+            ``ExpSquaredKernel`` in George
+
+        fit_model, str
+            Type of model to use for mean fitting, Optional, defaults
+            to none which is a pure GP modelling. Options: "linear" and
+            "polynomial"
+
+        lasso_model_alpha, float
+            Alpha for the Lasso model (only used of course when asking to
+            ``fit_linear_model``). If this is 0.0 (the default) basic linear
+            regression is used.
+
+        polynomial_degree, int
+            Maximal degree of the polynomial surface, default 1; linear for each
+            parameter
+
+        hide_progress: bool
+            Option to display a tqdm bar when creating the emulators,
+            Default is hide progress bar
         """
 
         model_values = self.model_values
@@ -75,7 +103,7 @@ class CrossCheckBins(object):
 
         emulators = {}
 
-        for unique_identifier in tqdm(self.leave_out_order):
+        for unique_identifier in tqdm(self.leave_out_order, disable=hide_progress):
             left_out_data = model_values.model_values.pop(unique_identifier)
 
             emulator = GaussianProcessEmulatorBins(
@@ -100,7 +128,7 @@ class CrossCheckBins(object):
 
         return
 
-    def plot_results(self, foldername: str = None):
+    def plot_results(self, output_path: Optional[Union[str, Path]] = None):
         """
         Make a plot of each of the leave_out emulators vs
         the original data.
@@ -116,7 +144,7 @@ class CrossCheckBins(object):
             the figures.
         """
         for unique_identifier in self.cross_emulators.keys():
-            fig, ax = plt.subplots(constrained_layout=True)
+            fig, ax = plt.subplots()
 
             emulate_at, emulated, emulated_error = self.cross_emulators[
                 unique_identifier
@@ -156,9 +184,14 @@ class CrossCheckBins(object):
             if foldername is None:
                 plt.show()
             else:
-                plt.savefig(foldername + f"/leave_out_{unique_identifier}.png")
+                plt.savefig(Path(output_path) / f"leave_out_{unique_identifier}.png")
 
-    def get_mean_squared(self, use_dependent_error: bool = False):
+    def get_mean_squared(
+        self,
+        use_dependent_error: bool = False,
+        use_y_as_error: bool = False,
+        use_squared_difference: bool = True,
+    ):
         """
         Calculates the mean squared per simulation and the total mean squared
         of the entire set of left-out simulations.
@@ -167,6 +200,13 @@ class CrossCheckBins(object):
         ----------
 
         use_dependent_error: bool
+            Use the simulation errors as weights for the mean squared calulation.
+            Default is false.
+
+        use_y_as_error: boolean
+            Use the model y values as the weights for the calculation.
+
+        use_squared_difference: boolean
             Use the simulation errors as weights for the mean squared calulation.
             Default is false.
         """
@@ -182,18 +222,24 @@ class CrossCheckBins(object):
                 ],
             )
 
-            emulated = emulated[:len(x_model)]
+            emulated = emulated[: len(x_model)]
 
-            if use_dependent_error:
+            if use_y_as_error:
+                y_model_error = y_model
+            else:
                 y_model_error = self.model_values.model_values[unique_identifier][
                     "dependent_error"
                 ]
-                uniq_mean_squared = ((y_model - emulated) / y_model_error) ** 2
+
+            if use_dependent_error:
+                uniq_mean_squared = (y_model - emulated) / y_model_error
             else:
-                uniq_mean_squared = (y_model - emulated) ** 2
+                uniq_mean_squared = y_model - emulated
+
+            if use_squared_difference:
+                uniq_mean_squared = uniq_mean_squared ** 2
 
             mean_squared_dict[unique_identifier] = uniq_mean_squared
-            total_mean_squared = np.append(
-                total_mean_squared, uniq_mean_squared)
+            total_mean_squared = np.append(total_mean_squared, uniq_mean_squared)
 
         return np.mean(total_mean_squared), mean_squared_dict
