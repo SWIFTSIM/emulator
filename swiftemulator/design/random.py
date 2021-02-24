ce32352f61e49d8551daf525218258587b3ef1e8
Josh Borrow <joshua.borrow@durham.ac.uk>
2021-02-24 15:38:30 +0000

    Re-worked hypercube design and added random design

--- swiftemulator/design/random.py
+++ swiftemulator/design/random.py
@@ -1,29 +1,23 @@
 """
-Generates a latin hypercube ``ModelValues`` container given
-the ``ModelSpecification``. Uses :mod:``pyDOE``.
+Generates a completely random design, by using the numpy random command.
 """
-
-from pyDOE import lhs
 
 from swiftemulator.backend.model_specification import ModelSpecification
 from swiftemulator.backend.model_parameters import ModelParameters
 from swiftemulator.design.transform import transform_to_model_spec
 
 from typing import Optional, Dict, Any, Optional
-from contextlib import redirect_stdout
-from os import devnull
 
 import numpy as np
 
 
-def create_hypercube(
+def create_cube(
     model_specification: ModelSpecification,
     number_of_samples: int,
-    correlation_retries: Optional[int] = 32,
     prefix_unique_id: Optional[str] = None,
 ) -> ModelParameters:
     """
-    Creates a Latin Hypercube model design.
+    Creates a random hypercube model design.
 
     Parameters
     ----------
@@ -35,11 +29,6 @@ def create_hypercube(
     number_of_samples: int
         The number of samples to draw; this will be the number
         of input simulations that you wish to create.
-
-    correlation_retries: int, optional
-        Number of times to re-try creating a random hypercube, to
-        minimize the correlation coefficient even further.
-        Default: 32.
 
     prefix_unique_id: str, optional
         An optional prefix for the newly generated unique IDs.
@@ -57,28 +46,13 @@ def create_hypercube(
     Notes
     -----
 
-    Uses :mod:``pyDOE``'s :func:`lhs` function, with the ``maximin``
-    method.
+    Uses numpy's random methods to generate a completely random (i.e.
+    no guarantee of a nice even distribution) hypercube
     """
 
-    samples = None
-    corr = 1.0
-
-    for _ in range(correlation_retries):
-        # Reduce correlation as much as practical
-        with redirect_stdout(open(devnull, "w")):
-            new_samples = lhs(
-                n=model_specification.number_of_parameters,
-                samples=number_of_samples,
-                criterion="maximin",
-            )
-
-        R = np.corrcoef(new_samples)
-        min_corr = np.max(np.abs(R - np.eye(R.shape[0])))
-
-        if min_corr <= corr:
-            samples = new_samples
-            corr = min_corr
+    samples = np.random.rand(
+        model_specification.number_of_parameters, number_of_samples
+    )
 
     return transform_to_model_spec(
         input_array=samples,
