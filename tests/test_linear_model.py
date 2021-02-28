306c8d4ec2153f2a41b12fdf009e22a389df1aa4
Josh Borrow <joshua.borrow@durham.ac.uk>
2021-02-28 20:22:25 +0000

    Adds a test for the new linear model emulator

---
+++ tests/test_linear_model.py
@@ -0,0 +1,64 @@
+"""
+Tests the ``LinearModelEmulator`` object.
+"""
+
+from swiftemulator.emulators.linear_model import LinearModelEmulator
+
+from swiftemulator.backend.model_parameters import ModelParameters
+from swiftemulator.backend.model_values import ModelValues
+from swiftemulator.backend.model_specification import ModelSpecification
+
+import numpy as np
+
+
+def test_linear_model():
+    """
+    A basic crash-or-not test for the Linear Model Emulator.
+    """
+
+    emulator = LinearModelEmulator(lasso_model_alpha=0.0)
+
+    model_spec = ModelSpecification(
+        number_of_parameters=2,
+        parameter_names=["x", "y"],
+        parameter_limits=[[1.0, 3.0], [0.0, 1.0]],
+    )
+
+    my_model_parameters = {
+        0: {
+            "x": 1,
+            "y": 1,
+        },
+        1: {
+            "x": 2,
+            "y": 1,
+        },
+        2: {"x": 3, "y": 1},
+    }
+
+    model_parameters = ModelParameters(model_parameters=my_model_parameters)
+
+    input_model_values = {
+        0: {
+            "independent": np.arange(10),
+            "dependent": np.random.rand(10),
+            "dependent_error": np.random.rand(10),
+        },
+        1: {
+            "independent": np.arange(10),
+            "dependent": np.random.rand(10),
+            "dependent_error": np.random.rand(10),
+        },
+        2: {
+            "independent": np.arange(10),
+            "dependent": np.random.rand(10),
+        },
+    }
+
+    model_values = ModelValues(model_values=input_model_values)
+
+    emulator.fit_model(
+        model_specification=model_spec,
+        model_parameters=model_parameters,
+        model_values=model_values,
+    )
