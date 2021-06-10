8bc7ce81e2ce6e5981e21553ca9e92fe579d8b9e
Josh Borrow <joshua.borrow@durham.ac.uk>
2021-06-10 10:17:59 +0100

    Add test for i/o

--- tests/test_io.py
+++ tests/test_io.py
@@ -5,7 +5,7 @@ Tests ancillary functions in the i/o submodule.
 import numpy as np
 
 from swiftemulator import ModelValues
-from swiftemulator.io.error import convert_dependent_error_to_standard_error
+from swiftemulator.io.error import convert_dependent_error_to_standard_error, convert_dependent_error_to_standard_error_using_edges
 
 
 def test_dependent_error():
@@ -45,6 +45,33 @@ def test_dependent_error():
     log = convert_dependent_error_to_standard_error(
         scaling_relation=model,
         histogram=histogram,
+        log_dependent=True,
+        log_independent=True,
+    )
+
+    return
+
+
+def test_dependent_error_individual():
+    scaling_relation = np.random.rand(16)
+    scaling_relation_bin_edges = np.linspace(0, 1, 17)
+    histogram = 10 * np.random.rand(8)
+    histogram_bin_edges = np.linspace(0.2, 0.8, 9)
+
+    convert_dependent_error_to_standard_error_using_edges(
+        scaling_relation,
+        scaling_relation_bin_edges,
+        histogram,
+        histogram_bin_edges,
+        log_dependent=False,
+        log_independent=False,
+    )
+
+    convert_dependent_error_to_standard_error_using_edges(
+        scaling_relation,
+        scaling_relation_bin_edges,
+        histogram,
+        histogram_bin_edges,
         log_dependent=True,
         log_independent=True,
     )
