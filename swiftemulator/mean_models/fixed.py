f788cfb3678bfe75bf67dabdb9e0e64138b94d21
Josh Borrow <josh@joshborrow.com>
2024-08-07 10:28:21 -0400

    Create fixed.py

---
+++ swiftemulator/mean_models/fixed.py
@@ -0,0 +1,38 @@
+"""
+Fixed mean model. Basic, fixed, 0th order model.
+"""
+
+from .base import MeanModel
+
+import numpy as np
+import attr
+
+from typing import Optional
+
+
+@attr.s
+class FixedMeanModel(MeanModel):
+    """
+    A basic offset mean model. Uses a fixed (manual) value for the mean
+    model and does not allow it to be changed.
+    """
+
+    model: float
+
+    def train(self, independent: np.ndarray, dependent: np.ndarray) -> None:
+        """
+        Train the model. See :class:`MeanModel` for more information.
+        """
+
+        # This is a no-op function because the model is fixed.
+
+        return
+
+    def predict(self, independent: np.ndarray) -> np.ndarray:
+        """
+        Predict using the model. See :class:`MeanModel` for more information.
+        """
+
+        dependent = np.ones(independent.shape[0], dtype=independent.dtype) * self.model
+
+        return dependent
